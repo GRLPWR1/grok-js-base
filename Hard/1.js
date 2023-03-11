96c71a01558db90a4be8eed7e02e9aa64bf440f4
GRLPWR1 <110125337+GRLPWR1@users.noreply.github.com>
2023-03-12 01:12:16 +0300

    solved Hard 1

--- Hard/1.js
+++ Hard/1.js
@@ -11,9 +11,11 @@
 
 */
 
-const validTriangle = () => {
-    /* Тут код*/
+const validTriangle = (a, b, c) => {
+    if (a < (b+c) && b < (a+c) && c < (a+b)){
+        return true;
+    } else { return false;}
 
 }
 
-module.exports = validTriangle
+// module.exports = validTriangle
